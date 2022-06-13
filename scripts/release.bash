84ced6bee29af69c3bae29fb4b519c7c64127984
Adrian <adrianrlc99@gmail.com>
2022-06-13 01:00:44 -0400

    add release script

---
+++ scripts/release.bash
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+repo='adrianrl99/space-invaders.rkt'
+tag=$1
+
+if [[ -z "$tag" ]]; then
+  echo "Tag missing"
+fi
+
+gh release create "$tag" -t "$tag" -n "$tag" -R "$repo" "./build/space-invaders"
